256b974f7dfbaebfd851a6b731e7c012519373c4
Malachi <the.sentry.guy.sl@gmail.com>
2024-05-13 22:12:46 +0200

    AV-106 Prepare for recaptcha

--- routes/UsersRoutes.js
+++ routes/UsersRoutes.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var UsersController = require('../controllers/UsersController.js');
-
+var Recaptcha = require('express-recaptcha').RecaptchaV3
+var recaptcha = new Recaptcha('6LcqTtspAAAAAE0QJruQ6T5V_jegb87IZmrEuLRQ', '6LcqTtspAAAAAMkultau9skecTPiDCAaB-uqdYF0')
 
 router.get('/', UsersController.list);
 
@@ -9,7 +10,7 @@ router.get('/', UsersController.list);
 router.get('/:id', UsersController.show);
 
 
-router.post('/', UsersController.create);
+router.post('/', recaptcha.middleware.verify, UsersController.create);
 
 
 router.put('/:id', UsersController.update);
