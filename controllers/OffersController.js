ddbbff816c6a55a69998be098f08a3261d66f799
lazevaa <lazeva152@gmail.com>
2024-05-16 09:03:55 +0200

    AV-84 Funkcija za dodajanje slik na streznik

--- controllers/OffersController.js
+++ controllers/OffersController.js
@@ -1,5 +1,18 @@
 var OffersModel = require('../models/OffersModel.js');
 const NodeGeocoder = require('node-geocoder');
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/images/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage });
+
 const options = {
     provider: 'openstreetmap'
 };
